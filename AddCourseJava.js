646012e5239598685a00a74d2ac30f516e2bf4ac
AJ Prescott <124528049+AJisHere88@users.noreply.github.com>
2023-10-05 18:37:58 -0500

    add course files uploaded

---
+++ AddCourseJava.js
@@ -0,0 +1,39 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ * Code by AJ Prescott
+ */
+
+var $ = function (id) {
+    return document.getElementById(id);
+};
+
+window.onload = function() {
+    $("CourseClearButton").onclick = fnClear;
+    $("CourseSubmitButton").onclick = fnSubmit;
+    $("courseCode").focus();
+};
+
+function fnClear() {
+    $("addCourseForm").reset();
+    $("CourseError").innerHTML = "";
+    $("courseCode").focus();
+};
+
+function fnSubmit() {
+    var iremoveAll = document.getElementById("allCourseInfo");
+    var code = $("courseCode").value.trim();
+    var name = $("courseName").value.trim();
+    var instructorid = $("instructorID").value.trim();
+    var start = $("startTime").value.trim();
+    var end = $("endTime").value.trim();
+    if (code === "" || name === "" || instructorid === "" || start === "" || 
+        end === "") {
+        $("CourseError").innerHTML = "All areas must be filled.";
+        $("CourseSuccess").innerHTML = "";
+    } else {
+        iremoveAll.parentNode.removeChild(iremoveAll);
+        $("CourseSuccess").innerHTML = "Your course was successfully added!";
+    }
+        
+};
